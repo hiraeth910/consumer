0c71b52dda17ab05309a9db3444267d755360415
gnani <head.telemoni@gmail.com>
2024-11-09 23:02:42 +0530

    pglinking

--- src/screens/Success.jsx
+++ src/screens/Success.jsx
@@ -1,12 +1,41 @@
-import './success.css'
-import success from '../assets/success.png'
-function Succes() {
+import './success.css';
+import success from '../assets/success.png';
+import { useEffect, useState } from 'react';
+
+// eslint-disable-next-line react/prop-types
+function Succes({ transactionId }) {
+  const [responseData, setResponseData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(`https://server.telemoni.in/api/payment/callback?tid=${transactionId}`);
+        const data = await res.text();
+        setResponseData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (transactionId) {
+      fetchData();
+    }
+  }, [transactionId]);
+
   return (
     <div className="success">
-      <div className="inner">      <img src={success} className='successimage'/>
-      <h3>Payment successful well deliver your product to your number.</h3>
-</div>
+      <div className="inner">
+        {loading ? (
+          <img src={success} className="successimage" alt="Success" />
+        ) : (
+          <p>{responseData}</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
-export default Succes
+
+export default Succes;
